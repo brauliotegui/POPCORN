791670fe705023aa5a4c32178e43d8efda59cf75
Braulio Ribeiro <brarrunategui@gmail.com>
2020-10-07 15:27:48 +0200

    working recommender

--- flask-app/application.py
+++ flask-app/application.py
@@ -1,54 +1,51 @@
 from flask import Flask, render_template, request
+import pickle5 as pickle
 from recommender import calculate_best_movies
 from recommender import similar_users_recommender
 from recommender import movieId_to_title
-import pickle5 as pickle
 
 import warnings
 warnings.filterwarnings("ignore")
 
 app = Flask(__name__)
-# make this file the center of the app
-# We launch this file from the terminal to start the app
 
 @app.route('/')
-@app.route('/index')  # whatever the function below this, route it to the path
+@app.route('/index', methods=['POST', 'GET'])
 def index():
-    movies=[]
+    movies = []
     return render_template('index.html')
 
 
-@app.route('/ratings', methods = ['POST','GET'])
+@app.route('/ratings', methods=['POST', 'GET'])
 def ratings():
-    user_input = dict(request.form)              # copied from github to make it worl
+    user_input = dict(request.form)
     print(user_input)
     if 'movielist' in user_input.keys():
         movielist = user_input['movielist']
         ids = list(map(int, movielist.split(',')))
         titles = movieId_to_title(ids)
-        movies = dict(zip(titles,ids))
+        movies = dict(zip(titles, ids))
     else:
         movies = dict()
     pkl_filename = "tmp.pkl"
     with open(pkl_filename, 'wb') as file:
         pickle.dump(movies, file)
     print(movies)
-    return render_template('ratings.html', movies_html=movies.items())
+    return render_template('ratings.html', movie_list=movies.items())
 
 
-
-@app.route('/recommendations')
+@app.route('/recommendations', methods=['GET', 'POST'])
 def recommender():
     with open("tmp.pkl", 'rb') as file:
         user_input_movies = pickle.load(file)
-    user_input_ratings = request.form.to_dict()
-    user_input = zip(user_input_movies, user_input_ratings.values())
-    result = calculate_best_movies(user_input)
-    return render_template('recommendations.html', result_html=result)
+    user_input_movies = list(user_input_movies)
+    user_input_ratings = request.args.getlist('rating_values')
+    user_input_ratings = [int(x) for x in user_input_ratings]
+    result = calculate_best_movies(user_input_movies, user_input_ratings)
+    result2 = similar_users_recommender(user_input_movies, user_input_ratings)
+    return render_template('recommendations.html', nmf=result, cosim=result2)
 
 if __name__ == '__main__':
-    # whatever occurs AFTER this line is executed when we run 'python application.py'
-    # however, whatever comes AFTER  this line is NOT executed when we IMPORT application.py
 
     app.run(debug = True)   # this will start an infinite process, i.e. serving our web page.
     #                        # 'debug = True' displays Back-End errors to the browser, which is useful for local development, NOT for production
--- flask-app/recommender.py
+++ flask-app/recommender.py
@@ -18,31 +18,45 @@ with open("nmf_model.pkl", 'rb') as file:
     m = pickle.load(file)
 P = m.components_
 
-def calculate_best_movies(result_html):
-    ''' doc '''
-    column_names = ['title', 'rating']
-    user_input = pd.DataFrame(result_html, columns=column_names)
+def calculate_best_movies(movies, ratings):
+    '''
+    uses a predifined NMF model to predict movies most likely to fit with the input movies and ratings
+
+    input:
+        the movies and ratings provided on the index- and ratings-html sites
+    output:
+        5 new movies the user likely to enjoy
+    '''
+    user = {'title' : movies, 'rating' : ratings}
+    user = pd.DataFrame(user)
 
     r_true = DF.pivot(index='userId', columns='movieId', values='rating')
     r_true.fillna(2.5, inplace=True)
 
-
-    user_ratings = pd.merge(MOVIES_DF, user_input, left_on='title', right_on='title', how='left')
+    user_ratings = pd.merge(MOVIES_DF, user, left_on='title', right_on='title', how='left')
     new_user = user_ratings['rating'].fillna(2.5)
     new_u = np.array(new_user).reshape(1, -1)
     profile = m.transform(new_u)
     result = np.dot(profile, P)
     MOVIES_DF['recom'] = result.T
 
-    result = MOVIES_DF.sort_values('recom', ascending=False)['title'].head(5)
-    return result
+    rec_movies = MOVIES_DF.sort_values('recom', ascending=False)['title']
+    result = rec_movies[~rec_movies.isin(user['title'])].dropna()
 
-def similar_users_recommender(result_html):
-    ''' doc '''
-    column_names = ['title', 'rating']
-    user_input = pd.DataFrame(result_html, columns=column_names)
+    return result.head(5)
 
-    user_ratings = pd.merge(MOVIES_DF, user_input, left_on='title', right_on='title', how='left')
+def similar_users_recommender(movies, ratings):
+    '''
+    uses a cosime-similarity matrix to predict movies most likely to fit with the input movies and ratings
+
+    input:
+        the movies and ratings provided on the index- and ratings-html sites
+    output:
+        5 new movies the user likely to enjoy
+    '''
+    user = {'title' : movies, 'rating' : ratings}
+    user = pd.DataFrame(user)
+    user_ratings = pd.merge(MOVIES_DF, user, left_on='title', right_on='title', how='left')
     query = user_ratings['rating']
     query = np.array(query)
 
@@ -53,7 +67,7 @@ def similar_users_recommender(result_html):
 
     cosim = cosine_similarity(m_matrix)[-1]
     cosim = pd.DataFrame(cosim)
-    top10 = cosim.sort_values(by=[0], ascending=[False]).head(11)  #order by most similar users
+    top10 = cosim.sort_values(by=[0], ascending=[False]).head(11)
     similar_users = list(top10.index)
     similar_users = similar_users[1:]
 
@@ -61,11 +75,11 @@ def similar_users_recommender(result_html):
     movie_ratings_avg = users_r.mean()
     movie_ratings_avg = pd.DataFrame(movie_ratings_avg)
 
-    rec_movies = movie_ratings_avg.sort_values(by=[0], ascending=[False]).head(10)
+    rec_movies = movie_ratings_avg.sort_values(by=[0], ascending=[False])
     rec_movies = pd.merge(rec_movies, MOVIES, left_on='movieId', right_on='movieId', how='left')
-
+    rec_movies[~rec_movies.isin(user['title'])].dropna()
     result = rec_movies['title']
-    return result
+    return result.head(5)
 
 def movieId_to_title(ids):
     ''' Given a list of movieIds, returns a corresponding list of movie titles.'''
